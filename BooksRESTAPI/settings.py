b2ac4667d6dd18b0852070a8ce3640e9fbf0660a
Admin <karolinaoparczyk@gmail.com>
2020-08-07 12:43:43 +0200

    Heroku deploy

--- BooksRESTAPI/settings.py
+++ BooksRESTAPI/settings.py
@@ -97,6 +97,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'ENGINE': 'django.db.backends.mysql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
@@ -163,5 +164,3 @@ django_heroku.settings(locals())
 import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
-
-#from .local_settings import *
