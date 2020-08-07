78cff10cbe47c4d303f5e1c039897462cf6e1cce
Admin <karolinaoparczyk@gmail.com>
2020-08-07 13:12:10 +0200

    Heroku deploy

--- BooksRESTAPI/settings.py
+++ BooksRESTAPI/settings.py
@@ -149,11 +149,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, '/books/static')
+STATIC_URL = '/books/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'books/static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
